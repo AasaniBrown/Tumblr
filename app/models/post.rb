41d89f10d2b2592e30f57f7cd1497331f4e2ec67
Aasani Brown <aasani.brown@hgs.hiddengeniusproject.org>
2020-02-25 17:10:55 -0800

    Add Pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
-class Post < ApplicationRecord
-    has_many :comments, :dependent :destroy
+class Post < ActiveRecord::Base
+    has_many  :comments, dependent: :destroy
     validates :title, presence: true, length: { minimum: 5 }
     validates :body, presence: true
-end
+  end
